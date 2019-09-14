d6d4472efb3374aa46e0a62e2325bf85aa8040dd
Рафаэль <rafkadyrov@gmail.com>
2019-09-15 00:46:58 +0300

    jk

---
+++ main.py
@@ -0,0 +1,18 @@
+from vk_api.vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll
+import random
+vk_session = VkApi(token='cffeaea983141ec8773bd5297a11ac46f64dfb9f4357d9ba71d97b5ad5e0032aea23e9564b9ae207552c6')
+api = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, 186515839)
+for event in longpoll.listen():
+    event = event.object
+    print(event)
+    id = event['from_id']
+    name = api.users.get(user_ids=id)[0]['first_name']
+    api.messages.send(
+
+        peer_id=event['peer_id'],
+        message=f"{name}, и это ты высрал?",
+        random_id=random.randint(0, 99999999999),
+        attachment='photo36066442_457241712_66edd3998cfe69ef1b'
+    )
